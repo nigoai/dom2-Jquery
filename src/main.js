a8a76931edc6ccd0cc3cff742a23661f5c13e4af
feng <1370261058@qq.com>
2022-01-05 16:13:27 +0800

    dom2

---
+++ src/main.js
@@ -0,0 +1,11 @@
+const x = jQuery('.test')
+// .find('.child')
+// .addClass('red')
+// .addClass('blue')  //不反回元素们，返回API 对象
+// .addClass('green')
+// .end()
+// .addClass('yellow')
+// x.each((div) => console.log(div))
+
+
+x.children().print()
